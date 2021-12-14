3a91f44cf7a9a2a50e11a40a30fe4872e8008603
IrfanCSE <irfanulhasan1996@gmail.com>
2021-12-14 17:14:32 +0600

    Add Store Procedure helper

---
+++ StoreProcedure/KeyValue.cs
@@ -0,0 +1,9 @@
+using System;
+namespace StoreProcedure
+{
+    public class KeyValue
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ StoreProcedure/StoreProcedure.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace StoreProcedure
+{
+    public static class StoreProcedure<T> where T : DbContext
+    {
+        private static T _contextW;
+        static StoreProcedure()
+        {
+            if (_contextW == null)
+                _contextW = (T)Activator.CreateInstance(typeof(T));
+        }
+        public static List<KeyValue> PostJson(string StoredProcedure, List<KeyValue> JsonObject, List<KeyValue> Output)
+        {
+            SqlConnection conn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+
+            conn.ConnectionString = _contextW.Database.GetDbConnection().ConnectionString;
+            // conn.ConnectionString = _contextW.Database.GetConnectionString();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = StoredProcedure;
+
+            JsonObject.ForEach(x =>
+            {
+                var json = JsonConvert.SerializeObject(x.Value);
+                var name = x.Key;
+                cmd.Parameters.AddWithValue(name, json);
+            });
+
+            foreach (var o in Output)
+            {
+                var typeName = o.Key.GetType().Name;
+                SqlDbType type = SqlDbType.VarChar;
+
+                if (typeName == "String") type = SqlDbType.VarChar;
+                else if (typeName == "Int32") type = SqlDbType.Int;
+
+                cmd.Parameters.Add(o.Key, type, 100);
+                cmd.Parameters[o.Key].Direction = ParameterDirection.Output;
+            }
+
+            conn.Open();
+            int i = cmd.ExecuteNonQuery();
+
+            foreach (var o in Output)
+            {
+                o.Value = cmd.Parameters[o.Key].Value;
+            }
+
+            conn.Close();
+
+            return Output;
+        }
+    }
+}
