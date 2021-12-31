55a75577c55bad2f5bb05c6408cb2049bb073a8b
Irfan <irfanulhasan1996@gmail.com>
2022-01-01 01:05:03 +0600

    null check

--- StoreProcedure/StoreProcedure.cs
+++ StoreProcedure/StoreProcedure.cs
@@ -9,53 +9,55 @@ namespace StoreProcedure
 {
     public static class StoreProcedure<T> where T : DbContext
     {
-        private static T _contextW;
+        private static T _context;
         static StoreProcedure()
         {
-            // TODO:: Check for self Context
-            if (_contextW == null)
-                _contextW = (T)Activator.CreateInstance(typeof(T));
+            if (_context == null)
+                _context = (T)Activator.CreateInstance(typeof(T));
         }
         public static Output PostJson(string Store_Procedure_Name, Json Object_To_Json, Output Output)
         {
             var connection = new SqlConnection();
             var command = new SqlCommand();
 
-            connection.ConnectionString = _contextW.Database.GetDbConnection().ConnectionString;
+            connection.ConnectionString = _context.Database.GetDbConnection().ConnectionString;
 
             command.Connection = connection;
             command.CommandType = CommandType.StoredProcedure;
             command.CommandText = Store_Procedure_Name;
 
-            Object_To_Json.ForEach(x =>
-            {
-                var json = JsonConvert.SerializeObject(x.Value);
-                var name = x.Key;
-                command.Parameters.AddWithValue(name, json);
-            });
+            if (Object_To_Json != null)
+                Object_To_Json.ForEach(x =>
+                {
+                    var json = JsonConvert.SerializeObject(x.Value);
+                    var name = x.Key;
+                    command.Parameters.AddWithValue(name, json);
+                });
 
-            foreach (var o in Output)
-            {
-                SqlDbType type = default;
+            if (Output != null)
+                foreach (var o in Output)
+                {
+                    SqlDbType type = default;
 
-                var typeName = o.Key.GetType().Name;
+                    var typeName = o.Key.GetType().Name;
 
-                // TODO:: need to add more type check
-                if (typeName == "String") type = SqlDbType.VarChar;
-                else if (typeName == "Int32") type = SqlDbType.Int;
+                    // TODO:: need to add more type check
+                    if (typeName == "String") type = SqlDbType.VarChar;
+                    else if (typeName == "Int32") type = SqlDbType.Int;
 
-                command.Parameters.Add(o.Key, type, 100);
-                command.Parameters[o.Key].Direction = ParameterDirection.Output;
-            }
+                    command.Parameters.Add(o.Key, type, 100);
+                    command.Parameters[o.Key].Direction = ParameterDirection.Output;
+                }
 
             connection.Open();
 
             var i = command.ExecuteNonQuery();
 
-            foreach (var o in Output)
-            {
-                o.Value = command.Parameters[o.Key].Value;
-            }
+            if (Output != null)
+                foreach (var o in Output)
+                {
+                    o.Value = command.Parameters[o.Key].Value;
+                }
 
             connection.Close();
 
--- test/Program.cs
+++ test/Program.cs
@@ -15,19 +15,24 @@ namespace test
         {
             using (var context = new AppDbContext())
             {
-                var obj = new { sms = "irfan" };
+                var obj = new { sms = "irfan",sms2 = "ul" };
+                var obj2 = new { sms = "ul",sms2 = "fsfa" };
 
                 var sp = "dbo.spPostJson";
                 var json = new Json();
                 json.Add("@Json", obj);
+                json.Add("@Json2", obj2);
                 var output = new Output();
                 output.Add("@Output");
 
                 var result = StoreProcedure<AppDbContext>.PostJson(sp, json, output);
+                var result2 = StoreProcedure<AppDbContextcopy>.PostJson(sp, json, output);
 
                 var r = result.Find(x => x.Key == "@Output").Value.ToString();
+                var r2 = result2.Find(x => x.Key == "@Output").Value.ToString();
 
                 System.Console.WriteLine($"Output is :-> {r}");
+                System.Console.WriteLine($"Output is :-> {r2}");
             }
         }
         #endregion
